c65884d336058162dad93ff773d39b10050f3d0b
Zhanarys Sairanbekov <arsik0153@gmail.com>
2019-06-22 23:03:17 +0600

    Basket add

---
+++ cabinet.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<!--[if lt IE 9 ]><html class="no-js oldie" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>HFF - Регистрация</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/vendor.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <style type="text/css" media="screen">
+        .s-styles { 
+            background: white;
+            padding-top: 15rem;
+            padding-bottom: 12rem;
+        }
+
+        .basket-wrap{
+          display: flex;
+          justify-content: space-around;
+          flex-wrap: wrap;
+          width: 94%;
+          margin: 0 auto;
+        }
+
+        .basket-block{
+          width: 300px;
+          background-color: rgb(245, 245, 245);
+          display: flex;
+          flex-direction: column;
+          align-content: center;
+          height: 200px;
+          text-align: center;
+          margin-top: 25px;
+        }
+     </style> 
+
+    <!-- script
+    ================================================== -->
+    <script src="js/modernizr.js"></script>
+    <script src="js/pace.min.js"></script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+</head>
+
+<body id="top">
+
+    <!-- header
+    ================================================== -->
+    <header class="s-header">
+
+        <div class="header-logo">
+            <a class="site-logo" href="index.html">
+                <img src="images/logo.png" alt="Homepage">
+            </a>
+        </div>
+
+    </header> <!-- end s-header -->
+
+
+    <!-- styles
+    ================================================== -->
+    <section id="styles" class="s-styles">
+        
+        <div class="row narrow section-intro add-bottom text-center">
+
+            <div class="col-twelve tab-full">
+
+                <h1 class="display-2">Корзина</h1>
+
+                <p class="lead">Берите любой товар, и они автоматически будут добавлены в корзину</p>
+
+            </div>
+
+        </div>
+
+        <div class="basket-wrap">
+
+          <div class="basket-block">
+            <h3>Говяжий доширак</h3>
+            <p>500 тенге</p>
+          </div>
+
+          <div class="basket-block">
+              <h3>Биг Бон куриный</h3>
+              <p>350 тенге</p>
+            </div>
+
+            <div class="basket-block">
+                <h3>Ролтон с перцем</h3>
+                <p>220 тенге</p>
+              </div>
+
+        </div> <!-- end row -->
+
+        
+
+      </section>
+
+
+
+    <!-- footer
+    ================================================== -->
+    <footer>
+
+                <div class="copyright">
+                    <span>© Copyright HFF 2019</span>
+                </div>
+
+                <div class="go-top">
+                    <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon-arrow-up" aria-hidden="true"></i></a>
+                </div>
+
+    </footer> <!-- end footer -->
+
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader">
+        <div id="loader">
+            <div class="line-scale-pulse-out">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
